28f7b5eb897055c4e6dc5fca19af495e3a459d30
Stance1234 <111715127+Stance1234@users.noreply.github.com>
2025-03-02 11:15:40 +0100

    smarts up removed

--- src/component/smartsup.js
+++ src/component/smartsup.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const SmartsuppChat = () => {
   useEffect(() => {
     window._smartsupp = window._smartsupp || {};
-    window._smartsupp.key = "5e4385baf8c0e2eff416fddf66f6bb1afd621bb5";
+    window._smartsupp.key = "";
 
     if (!document.getElementById("smartsupp-script")) {
       const script = document.createElement("script");
